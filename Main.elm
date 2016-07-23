d4801aba975f078fbf8dfed54e2ecf04476bdc37
Christopher Nyberg <christopher.nyberg@gmail.com>
2016-07-22 20:34:16 -0700

    Made input field
    Still need to clear the input field.

--- Main.elm
+++ Main.elm
@@ -3,30 +3,45 @@ module Main exposing (..)
 import Html exposing (..)
 import Html.Attributes exposing (..)
 import Html.Events exposing (..)
+import Html.App as App
+import String exposing (toInt)
 
 
 type alias Model =
-    Int
+    { inputValue : Int
+    , total : Int
+    }
 
 
 initModel : Model
 initModel =
-    0
+    { inputValue = 0
+    , total = 0
+    }
 
 
 type Msg
     = AddCalorie
     | Clear
+    | Change String
 
 
 update : Msg -> Model -> Model
 update msg model =
     case msg of
         AddCalorie ->
-            model + 1
+            { model | total = model.total + model.inputValue }
 
         Clear ->
-            0
+            { model | total = 0 }
+
+        Change num ->
+            case (String.toInt num) of
+                Ok val ->
+                    { model | inputValue = val }
+
+                Err _ ->
+                    model
 
 
 view : Model -> Html Msg
@@ -34,7 +49,10 @@ view model =
     div []
         [ h3
             []
-            [ text ("Total Calories: " ++ toString model) ]
+            [ text ("Total Calories: " ++ toString model.total) ]
+        , input
+            [ placeholder "Enter Calories here: ", onInput Change ]
+            []
         , button
             [ type' "button", onClick AddCalorie ]
             [ text "Add" ]
@@ -42,3 +60,12 @@ view model =
             [ type' "button", onClick Clear ]
             [ text "Clear" ]
         ]
+
+
+main : Program Never
+main =
+    App.beginnerProgram
+        { model = initModel
+        , update = update
+        , view = view
+        }
